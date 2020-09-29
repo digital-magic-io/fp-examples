a9e3ba153bad9553a66963e8089e78f5b5be15b2
Jevgeni Goloborodko <jevgeni@digital-magic.io>
2020-09-29 13:28:40 +0300

    Added comments to make code more understandable

--- src/08.ts
+++ src/08.ts
@@ -2,16 +2,22 @@ import { Functor1 } from 'fp-ts/Functor'
 import { Eq, eqNumber, eqStrict, eqString, getStructEq } from 'fp-ts/Eq'
 import { getEq } from 'fp-ts/Record'
 
+// URI - keeps pair of TypeClass name and it's type (we'll use it for our functor)
 export const URI = 'Response'
-
 export type URI = typeof URI
 
+// TypeScript way to use Higher-Kinded Types. The HKT type represents a type constructor of a kind.
 declare module 'fp-ts/lib/HKT' {
+  // URItoKind is type-level map,
+  // it maps a URI to a concrete data type
+  // and is populated using the module augmentation feature:
+  // https://www.typescriptlang.org/docs/handbook/declaration-merging.html
   interface URItoKind<A> {
-    readonly Response: Response<A>
+    readonly Response: Response<A> // maps key 'Response' to type Response
   }
 }
 
+// Our object that will be used as an effect for A
 export interface Response<A> {
   readonly url: string
   readonly status: number
@@ -19,13 +25,15 @@ export interface Response<A> {
   readonly body: A
 }
 
-function mapResponse<A, B>(r: Response<A>, f: (a: A) => B): Response<B> {
+// Function that maps Response using function: f: A => B
+function map<A, B>(r: Response<A>, f: (a: A) => B): Response<B> {
   return { ...r, body: f(r.body) }
 }
 
+// Functor instance for Response
 const functorResponse: Functor1<URI> = {
   URI,
-  map: mapResponse
+  map
 }
 
 const resp: Response<string> = {
@@ -44,6 +52,7 @@ const eqResp: Eq<Response<any>> = getStructEq<Response<any>>({
   body: eqStrict
 })
 
+// Update initial Response using functor passing function to convert string body to it's length
 const targetResp: Response<number> = functorResponse.map(resp, body => body.length)
 
 const expectedResp: Response<number> = {
